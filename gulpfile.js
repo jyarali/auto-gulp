2c975a3a2a28a1d685819cbe1195d62174dab596
Jamal Yarali <jamaldanlod@gmail.com>
2018-08-12 22:28:09 +0430

    added suorcemaps, css and js minify, and auto inject css and js to index

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,46 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    autoprefixer = require('gulp-autoprefixer');
+    plugins = require('gulp-load-plugins')({
+        pattern: '*'
+    });
 var autoprefixerOptions = {
     browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
 };
-gulp.task('sass', function() {
-    gulp.src('./scss/*.scss')
-        .pipe(sass())
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest('./css/'))
-        .pipe(browserSync.stream());
+gulp.task('sass', function () {
+    gulp.src('./src/scss/*.scss')
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer(autoprefixerOptions))
+        .pipe(plugins.cleanCss())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(plugins.browserSync.stream());
 });
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('js', function () {
+    gulp.src('./src/js/*.js')
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(plugins.browserSync.stream());
+});
 
-    browserSync.init({
-        server: "./"
+gulp.task('index', function () {
+    var target = gulp.src('./index.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var sources = gulp.src(['./dist/**/*.js', './dist/**/*.css'], {
+        read: false
     });
 
-    gulp.watch("./scss/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
-    gulp.watch("./js/*.js").on('change', browserSync.reload);
+    target.pipe(plugins.inject(sources))
+        .pipe(gulp.dest('./'))
+        .pipe(plugins.browserSync.stream());
+});
+
+gulp.task('serve', ['sass', 'js', 'index'], function () {
+    plugins.browserSync.init({
+        server: "./"
+    });
+    gulp.watch("./src/scss/*.scss", ['sass']);
+    gulp.watch("./src/js/*.js", ['js']);
+    gulp.watch("./*.html", ['index']);
 });
 gulp.task('default', ['serve']);
